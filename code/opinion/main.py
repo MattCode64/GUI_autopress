dcbb03dda8214f7c0449c345a2ec4b19e1265479
MattKio <matthieu.freire.eleuterio@efrei.net>
2023-11-21 14:48:43 +0100

    DONE

---
+++ code/opinion/main.py
@@ -0,0 +1,39 @@
+from opinion import *
+from navigation import *
+from pdfconvert import *
+
+
+def main():
+    print("Start of program")
+    config = r"C:\Data\Projet CODE\Code Python\Présidence\Travail\RP AUTO PQN\data\config\config.json"
+
+    # Setup Driver
+    edge_driver = SetupDriver()
+
+    # Open Website
+    OpenWebsite(edge_driver, config, "lopinion")
+
+    # Accept Cookies
+    AcceptCookies(edge_driver)
+
+    # Switch to iframe
+    SwitchIframe(edge_driver)
+
+    # Sign In
+    SignIn(edge_driver, config, "lopinion")
+
+    # Read Mode
+    # ReadMode(edge_driver, config)
+    # time.sleep(5000)
+
+    Processing(edge_driver, config)
+
+    # Convert PDF
+    Conversion(config)
+
+    QuitDriver(edge_driver)
+    print("End of program")
+
+
+if __name__ == '__main__':
+    main()
