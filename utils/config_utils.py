711a2a41226bfbbb2d11bf5c2f136305ab79756b
MattKio <matthieu.freire.eleuterio@efrei.net>
2023-11-24 16:11:39 +0100

    function to access in json files depending on what we want

--- utils/config_utils.py
+++ utils/config_utils.py
@@ -2,33 +2,42 @@ import json
 import os
 
 
-
-def open_config(param):
-    with open(os.path.join(os.path.dirname(__file__), f'../../data/config/{param}.json'), "r") as f:
-        configJson = json.load(f)
-
-    return configJson
-
-
-def open_config_file(param):
-    with open(os.path.join(os.path.dirname(__file__), f'../../data/config/{param}.json'), "r") as f:
-        configJson = json.load(f)
-
-    return configJson
+def open_config(keyword):
+    try:
+        with open(os.path.join(os.path.dirname(__file__), f'../data/config/{keyword}.json'), "r") as f:
+            configJson = json.load(f)
+            print("GOOD in open_config in config_utils.py")
+            print(type(configJson))
+        return configJson
+    except Exception as e:
+        print(f"Error in open_config: {e}")
 
 
-def get_config_file(*args):
-    if any(keyword in args for keyword in ["url", "credentials", "pdf"]):
-        print("GOOD in first if")
-        # configJson = open_config("config")
-        # return configJson
+def open_config_file(media):
+    try:
+        with open(os.path.join(os.path.dirname(__file__), f'../data/config/{media}.json'), "r") as f:
+            configJson = json.load(f)
+            print("GOOD in open_config_file in config_utils.py")
+            print(type(configJson))
+        return configJson
+    except Exception as e:
+        print(f"Error in open_config_file: {e}")
 
-    elif any(media in args for media in ["lefigaro", "lacroix", "liberation"]):
-        # configJson = open_config_file(args[0])
-        print("GOOD in second if")
-        # return configJson
 
-    else:
-        print("Error while getting config file")
-        return
+def get_json_file(*args):
+    try:
+        if any(keyword in args for keyword in ["url", "credentials", "pdf"]):
+            print("GOOD in first if : keyword", args[0])
+            configJson = open_config("config")
+            return configJson
 
+        elif any(media in args for media in ["lefigaro", "lacroix", "liberation"]):
+            print("GOOD in second if : media", args[0])
+            configJson = open_config_file(args[0])
+            return configJson
+
+        else:
+            print("Error while getting config file")
+            return
+    except Exception as e:
+        print(f"Error in get_json_file: {e}")
